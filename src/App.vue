6c57cb4f43953c0e17ea39efb17ff46d38db3a6d
Damian Rosiak <rosiakd6@gmail.com>
2023-03-27 15:30:00 +0200

    posting game

--- src/App.vue
+++ src/App.vue
@@ -35,11 +35,7 @@ interface Games {
 
 const games = ref<Games[]>([])
 
-const game = ref({})
 const modalButton = ref(false)
-
-
-
 
 const search = async () => {
   games.value = []
@@ -87,7 +83,7 @@ onMounted(async () => {
 
 
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class=" overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
--- src/components/GameForm.vue
+++ src/components/GameForm.vue
@@ -51,14 +51,14 @@ const postGame = async() => {
                 </svg>
             </span>
 
-            <form>
+            <div>
                 <div class="mb-6">
                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                         Name
                     </label>
                     <input type="text" id="name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type name">
+                        placeholder="Type name" v-model="name">
                 </div>
                 <div class="mb-6">
                     <label for="platform" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -66,7 +66,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="platform"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type platform">
+                        placeholder="Type platform" v-model="platform">
                 </div>
                 <div class="mb-6">
                     <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -74,7 +74,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="year"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type year">
+                        placeholder="Type year" v-model="year">
                 </div>
                 <div class="mb-6">
                     <label for="genre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -82,7 +82,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="genre"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type genre">
+                        placeholder="Type genre" v-model="genre">
                 </div>
                 <div class="mb-6">
                     <label for="publisher" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -90,7 +90,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="publisher"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type publisher">
+                        placeholder="Type publisher" v-model="publisher">
                 </div>
                 <div class="mb-6">
                     <label for="na" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -98,7 +98,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="na"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type NA_Sales">
+                        placeholder="Type NA_Sales" v-model="naSales">
                 </div>
                 <div class="mb-6">
                     <label for="eu" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -106,7 +106,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="eu"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type EU_Sales">
+                        placeholder="Type EU_Sales" v-model="euSales">
                 </div>
                 <div class="mb-6">
                     <label for="jp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -114,7 +114,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="jp"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type JP_Sales">
+                        placeholder="Type JP_Sales" v-model="jpSales">
                 </div>
                 <div class="mb-6">
                     <label for="other" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -122,7 +122,7 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="other"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type Other_Sales">
+                        placeholder="Type Other_Sales" v-model="otherSales">
                 </div>
                 <div class="mb-6">
                     <label for="global" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -130,11 +130,11 @@ const postGame = async() => {
                     </label>
                     <input type="text" id="global"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Type Global_Sales">
+                        placeholder="Type Global_Sales" v-model="globalSales">
                 </div>
                 <button @click="postGame"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send game</button>
-            </form>
+            </div>
 
 
         </div>
