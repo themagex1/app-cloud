88239cd68d4aea5150bf486fbd32d9dcb1877103
Damian Rosiak <rosiakd6@gmail.com>
2023-03-27 16:14:38 +0200

    fix table

--- src/App.vue
+++ src/App.vue
@@ -88,9 +88,7 @@ onMounted(async () => {
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3">
-              Rank
-            </th>
+            
             <th scope="col" class="px-6 py-3">
               Name
             </th>
@@ -99,6 +97,9 @@ onMounted(async () => {
             </th>
             <th scope="col" class="px-6 py-3">
               Year
+            </th>
+            <th scope="col" class="px-6 py-3">
+              Genre
             </th>
             <th scope="col" class="px-6 py-3">
               Publisher
